44d63abdd8a8d6347c6e4c672a6ec3b8bebd3b4c
Todd Stoneberg <todd.stoneberg@gmail.com>
2021-02-01 18:27:08 -0700

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,12 @@
 class ProductsController < ApplicationController
     def index
-        @cart = session[:cart]
+        
     end
 
     def add
+        cart << params[:product]
         
+        render :index
     end
 
 end
