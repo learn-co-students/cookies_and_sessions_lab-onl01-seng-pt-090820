482e22f28edf72a1ef7de52eddcdfb3be896caba
DavidRMorphew <morphewd@umich.edu>
2021-02-08 18:23:34 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   
   # resources :products, only: [:index, :add]
 
+  # root 'products#index' This is an alternative way to set the root
+  
   get '/', to: 'products#index', as: "products"
   post '/', to: 'products#add'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
