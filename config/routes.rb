f044c336e8418672b8f05f2ff7a06ee94e0bd640
Carlj0666 <carlj1881@yahoo.com>
2021-02-08 21:24:32 -0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :basket, only: [:show, :update, :destroy]
+  
+  get '/', to: 'products#index'
+  get 'POST', to: 'products#cart'
+
 end
