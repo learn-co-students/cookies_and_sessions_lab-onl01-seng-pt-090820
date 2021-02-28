5f982ff870f6af9df48a0a3cb720df4f9dfc5cd6
musiccity365 <m_sackstein@yahoo.com>
2021-02-28 15:44:47 -0500

    commit tree dcc386f707b04a80d947d890794b0f09a3684ba3

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'sessions/destroy,'
+
+  get 'sessions/new,'
+
+  get 'sessions/create'
+
+  root 'application#hello'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
 end
